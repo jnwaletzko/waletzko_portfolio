4114f79247615ca093df74ee3c986fc8cd8b8bbf
jnwaletzko <jnwaletzko@gmail.com>
2016-02-04 12:25:23 -0600

    Update index tests

--- test/controllers/educations_controller_test.rb
+++ test/controllers/educations_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class EducationsControllerTest < ActionController::TestCase
-   test "the truth" do
-     assert true
-   end
+  test "should be succesful" do
+    get :index, :format => :json
+    assert_response :success
+  end
+
+  test "should return all educations" do
+    get :index, :format => :json
+    body = JSON.parse(response.body)
+    assert body.count, 2
+  end
+
+
 end
