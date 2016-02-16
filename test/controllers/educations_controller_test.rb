afc26b9052a7bf0a8990fd2bf773af053ec4f2f4
jnwaletzko <jnwaletzko@gmail.com>
2016-02-16 15:00:47 -0600

    Update index, show, and create controllers

--- test/controllers/educations_controller_test.rb
+++ test/controllers/educations_controller_test.rb
@@ -1,16 +1,31 @@
 require 'test_helper'
 
 class EducationsControllerTest < ActionController::TestCase
-  test "should be succesful" do
+  test "index should be successful" do
     get :index, :format => :json
     assert_response :success
   end
 
-  test "should return all educations" do
+  test "index should return all educations" do
     get :index, :format => :json
     body = JSON.parse(response.body)
     assert body.count, 2
   end
 
+  test "show should be successful" do
+    get :show, id: 1, :format => :json
+    assert_response :success
+  end
 
+  test "show should return first education" do
+    get :show, id: 1, :format => :json
+    body = JSON.parse(response.body)
+    assert body['title'], 'Bachelor of Arts'
+  end
+
+  test "create should create an education" do
+    assert_difference('Education.count') do
+      post :create, education: {title: "Test education", location: "test location", description: "test description"}, :format => :json
+    end
+  end
 end
