cdf21bbb0982fb1afbacbe6797922f7fee2d29f5
jnwaletzko <jnwaletzko@gmail.com>
2016-02-21 16:49:23 -0600

    Add first rspec test testing educations index with factory girl

--- spec/controllers/education_controller_spec.rb
+++ spec/controllers/education_controller_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe EducationsController do
 
+  let(:json) { JSON.parse(response.body) }
+  let!(:education) { create(:education) }
+
   describe 'index' do
     before(:each) do
       get :index, format: :json
     end
 
-    # it { expect(assigns(:educations)).to match_array educations }
+    it { expect(json.collect{|l| l["title"]}).to include(education.title) }
     it { expect(response).to be_success }
   end
 end
---
+++ spec/factories/educations.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :education do
+    title 'factory education'
+    location 'factory location'
+    description 'factory description'
+  end
+end
