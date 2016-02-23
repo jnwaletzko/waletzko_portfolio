182b9bd20c19d5ac48e55798707057f6c855fb4b
jnwaletzko <jnwaletzko@gmail.com>
2016-02-22 18:23:02 -0600

    finish converting education tests to rspec

--- spec/controllers/education_controller_spec.rb
+++ spec/controllers/education_controller_spec.rb
@@ -13,4 +13,25 @@ describe EducationsController do
     it { expect(json.collect{|l| l["title"]}).to include(education.title) }
     it { expect(response).to be_success }
   end
+
+  describe 'show' do
+    before(:each) do
+      get :show, id: education.id, format: :json
+    end
+
+    it { expect(response).to be_success }
+    it { expect(json['title']).to eq(education.title)}
+    it { expect(json['location']).to eq(education.location)}
+    it { expect(json['description']).to eq(education.description)}
+  end
+
+  describe 'create' do
+    context "with valid attributes" do
+      it "creates a new education" do
+        expect {
+          post :create, education: attributes_for(:education), :format => :json
+        }.to change(Education, :count).by(1)
+      end
+    end
+  end
 end
