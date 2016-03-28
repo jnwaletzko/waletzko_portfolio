eca3c47ef62633b60fb4a49b218349e459adb4be
jnwaletzko <jnwaletzko@gmail.com>
2016-03-27 19:48:50 -0500

    move skills as comment just in case

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,11 @@
+# {"name":"C#","level":"7"},
+# {"name":"VB.Net","level":"6"},
+# {"name":"Ruby","level":"4"},
+# {"name":"Rails","level":"4"},
+# {"name":"SQL","level":"6"},
+# {"name":"Javascript","level":"5"},
+# {"name":"WPF","level":"4"},
+# {"name":"Java","level":"3"}
+
 class Skill < ActiveRecord::Base
 end
